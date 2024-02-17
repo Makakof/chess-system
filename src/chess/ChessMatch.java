449e2f60817ad35e95bccd52678c341e6622b20f
Makakof <luiz.h.s.silva@ufms.br>
2024-02-17 14:43:41 -0300

    Created pieces Rook and King and the method to place in the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Rook;
 
 public class ChessMatch
 {
@@ -9,6 +11,7 @@ public class ChessMatch
     public ChessMatch()
     {
         board = new Board(8,8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces()
@@ -22,5 +25,10 @@ public class ChessMatch
             }
         }
         return mat;
+    }
+
+    private void initialSetup()
+    {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
     }
 }
