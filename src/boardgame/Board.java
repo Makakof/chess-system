fa8f55877d2a61243923c8008cda96d74e0c7950
Makakof <luiz.h.s.silva@ufms.br>
2024-02-18 20:04:15 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -52,6 +52,22 @@ public class Board
         piece.position = position;
     }
 
+    public Piece removePiece(Position position)
+    {
+        if(!positionExist(position))
+        {
+            throw new BoardException("Position not on the board");
+        }
+        if(piece(position) == null)
+        {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
+
     public boolean positionExist(int row, int column)
     {
         boolean verifyRowExist, verifyColumnExist;
